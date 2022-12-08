d38b0b2f7d9f490762c3ee3adc3d60af899c37a4
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2022-12-08 21:38:50 +0700

    test push

---
+++ Quang_Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    public String fullName;
+    public int age;
+    public int GPA;
+
+    public Student(String fullName, int age, int GPA) {
+        this.fullName = fullName;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
+}
