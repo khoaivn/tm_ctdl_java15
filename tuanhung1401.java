b6c991d26e8d0c08eff3e1f320ba190d270b89e4
tuanhung1401 <tuanhung1401@gmail.com>
2022-12-08 21:53:31 +0700

    tuanhung1401

---
+++ tuanhung1401.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("nguoi thu ");
+        int i = 0;
+        for ( int u = 0; u < 10; ++u) {
+            i = u+ u;
+            System.out.println("nguoi thu" + u);
+        }
+    }
+}
