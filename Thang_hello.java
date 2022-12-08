537529285b4cba9fb96175f743d32becd764b072
lucky2588 <duwcsthang2001@gmail.com>
2022-12-08 21:18:16 +0700

    no message

---
+++ Thang_hello.java
@@ -0,0 +1,11 @@
+package tm_ctdl_java15;
+public class Thang_hello{
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Test CTDL ");
+        System.out.println("Người thứ Năm");
+
+    }
+}
