c6c8010734ece3c34f2f212925b6c1631727ce85
lucky2588 <duwcsthang2001@gmail.com>
2022-12-13 16:56:43 +0700

    Update Homework_Thang.java
    update

--- Homework_Thang.java
+++ Homework_Thang.java
@@ -1,10 +1,12 @@
 package tm_ctdl_java15;
+
 public class Thang_hello {
     public static void main(String[] args) {
         System.out.println("Class luu bai tap ve nha B1");
         System.out.println("Ho va Ten : Nguyen Duc Thang");
     }
-                    // Buổi 1
+
+    // Buổi 1
     // 1. Two Sum link bài tập : https://leetcode.com/problems/two-sum/
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -24,7 +26,9 @@ public class Thang_hello {
         }
         return result;
     }
-    // B136 : Single Number link Bài tập : https://leetcode.com/problems/single-number/
+
+    // B136 : Single Number link Bài tập :
+    // https://leetcode.com/problems/single-number/
     public int singleNumber(int[] nums) {
         int isCheck = 0;
         int result = 0;
@@ -46,14 +50,18 @@ public class Thang_hello {
         }
         return result;
     }
-    // B2469 : Convert the Temperature link Bài Tập : https://leetcode.com/problems/convert-the-temperature
+
+    // B2469 : Convert the Temperature link Bài Tập :
+    // https://leetcode.com/problems/convert-the-temperature
     public double[] convertTemperature(double celsius) {
         double[] returnConvert = new double[2];
         returnConvert[0] = celsius + 273.15;
         returnConvert[1] = celsius * 1.80 + 32.00;
         return returnConvert;
     }
-    // Bài 1920. Build Array from Permutation link Bài Tập : https://leetcode.com/problems/build-array-from-permutation/
+
+    // Bài 1920. Build Array from Permutation link Bài Tập :
+    // https://leetcode.com/problems/build-array-from-permutation/
     public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
@@ -61,4 +69,35 @@ public class Thang_hello {
         }
         return ans;
     }
+
+    // Buổi 2 :
+    // B1480. Running Sum of 1d Array link
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+
+    // B387 : First Unique Character in a String
+    public int firstUniqChar(String s) {
+        int check = 0;
+        int result = -1;
+        endPro: for (int i = 0; i < s.length(); i++) {
+            check = 0;
+            char c = s.charAt(i);
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == c && i != j) {
+                    check += 1;
+                }
+            }
+            if (check == 0) {
+                result = i;
+                break endPro;
+            }
+
+        }
+        return result;
+    }
+
 }
