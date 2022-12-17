65240a326dff7948b787c17fbdbfdc9067a2f984
lucky2588 <duwcsthang2001@gmail.com>
2022-12-17 17:41:41 +0700

    Update Homework_Thang.java

--- Homework_Thang.java
+++ Homework_Thang.java
@@ -100,4 +100,124 @@ public class Thang_hello {
         return result;
     }
 
+    // Buổi 3 :
+    // B1051 : Height Checker link : https://leetcode.com/problems/height-checker/
+    public int heightChecker(int[] heights) {
+        int isCheck = 0;
+        int[] list = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            list[i] = heights[i];
+        }
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights.length - i - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    int tmp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != list[i]) {
+                isCheck += 1;
+            }
+        }
+        return isCheck;
+    }
+
+    // B217. Contains Duplicate
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // B2404. Most Frequent Even Element..
+    public int mostFrequentEven(int[] nums) {
+        Arrays.sort(nums);
+        int recordCount = 0;
+        int answer = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            if (nums[i] % 2 == 0) {
+                int count = 1;
+                while (i < nums.length - 1 && number == nums[i + 1]) {
+                    count++;
+                    i++;
+                }
+                if (count > recordCount) {
+                    answer = number;
+                    recordCount = count;
+                }
+            }
+        }
+        return answer;
+    }
+
+    // Buổi 4 : Đệ Quy
+    // B509 : Fibonacci Number
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n < 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // B231 : Power of Two
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1)
+            return true;
+        else {
+            return isCheck(2, n);
+        }
+    }
+
+    public boolean isCheck(long mu_hat, int n) {
+        if (mu_hat > n)
+            return false;
+        if (mu_hat == n)
+            return true;
+        else
+            return isCheck(mu_hat * 2, n);
+    }
+
+    // B326. Power of Three
+    public boolean isPowerOfThree(int n) {
+        if (n == 1)
+            return true;
+        if (n == 0)
+            return false;
+        return isCheck(3, n);
+    }
+
+    public boolean isCheck(long so_mu, int n) {
+        if (so_mu > n)
+            return false;
+        if (so_mu == n)
+            return true;
+        return isCheck(so_mu * 3, n);
+    }
+
+    // B342. Power of Four
+    public boolean isPowerOfFour(int n) {
+        if (n == 0)
+            return false;
+        if (n == 1)
+            return true;
+        if (n % 4 != 0)
+            return false;
+        return isPowerOfFour(n / 4);
+    }
+
 }
